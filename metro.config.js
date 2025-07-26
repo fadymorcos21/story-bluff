a5ed6dd585381643a5c98b41131975438bba4dc3
Fady Morcos <fadymorcos22@gmail.com>
2025-07-26 12:21:44 -0400

    attempting native wind build error fix

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,6 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer.minifierPath = "metro-minify-terser";
+
 module.exports = withNativeWind(config, { input: "./global.css" });
