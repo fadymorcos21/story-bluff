2fb50fc0d7ac9b5312ba38b644f171e858199b4c
Fady Morcos <fadymorcos22@gmail.com>
2025-07-26 14:14:29 -0400

    added plugin to remove dup pirvacy files

---
+++ app.config.js
@@ -0,0 +1,43 @@
+// app.config.js
+
+export default {
+  name: "story-bluff",
+  slug: "story-bluff",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "com.fadymorcos21.storybluff",
+    buildNumber: "1.0.14",
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false
+    }
+  },
+  android: {
+    versionCode: 1,
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    edgeToEdgeEnabled: true
+  },
+  plugins: [
+    "expo-router",
+    "./plugins/removeDuplicatePrivacyFiles" // 👈 plugin to fix xcprivacy duplication
+  ],
+  extra: {
+    BACKEND_URL: "https://resistnce-game-srver-app.store",
+    TEST_MODE: "FALSE",
+    router: {},
+    eas: {
+      projectId: "ca83fc9f-4f5a-4bda-ad14-43db8467352b"
+    }
+  }
+};
